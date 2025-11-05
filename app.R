25e1997d0122fb7fd4fb225923b94b0cab34cb04
Thad Creech <thcreech51@gmail.com>
2025-11-05 16:58:28 -0500

    Completed About tab funcitonality

--- app.R
+++ app.R
@@ -113,6 +113,63 @@ server <- function(input, output, session) {
     
   })
   
+  ### About Tab Functionality
+  output$about <- renderUI({
+    tagList(
+      br(),
+      tags$img(
+        src = "bank_image.jpg",
+        style = "max-width:100%; height:auto",
+      ),
+      h3("Purpose of this app"),
+      p("This app was created to allow users to explore the Bank Marketing dataset. The data is sourced from direct marketing campaigns of a Portuguese
+        banking institution. The app allows for users to subset data using two categorical and two numerical filters. Once data is subseted, the data can be downloaded as a CSV
+        or explored in the app."),
+      hr(),
+      h3("How to use it"),
+      h5("Side Panel"),
+      tags$ol(
+        tags$li(tags$b("Pick Filters:"), "Choose two categorical varialbes and two numerical variables you would like to subset the data with from the right side panel"),
+        tags$li(tags$b("Set Levels and Ranges:"), "Use the dropdowns and sliders to select the specific levels and ranges that you are interested in"),
+        tags$li(tags$b("Click Apply:"), "Once you are happy with your selection, click apply button to subset the data"),
+      ),
+      h5("Main Tabs"),
+      tags$ul(
+        tags$li(tags$b("Download:"), "Go to the Download tab if you would like to see the tabular data and/or download the subsetted data as a CSV"),
+        tags$li(tags$b("Explore:"), "Go to the Explore tab to access tables and graphs that are useful in exploring the subsetted date")
+      ),
+      hr(),
+      h3("The Data"),
+      p("Source: UCI/Kaggle Banking Dataset - Marketing Targets"),
+      tags$ul(
+        tags$li(tags$b("Age:"), "Age of campaign target"),
+        tags$li(tags$b("Job:"), "Job type of the campaign target"),
+        tags$li(tags$b("Marital:"), "Marital status of the campaign target"),
+        tags$li(tags$b("Education:"), "Education level of the campaign target"),
+        tags$li(tags$b("Default:"), "Whether or not the ccampaign target has credit in default"),
+        tags$li(tags$b("Balance:"), "Average yearly balance of campaign target, in euros"),
+        tags$li(tags$b("Housing:"), "Whether or not the campaign target has a housing loan"),
+        tags$li(tags$b("Loan:"), "Whether or not the campaign target has a personal loan"),
+        tags$li(tags$b("Contact:"), "Communication type of last contact of the current marketing campaign"),
+        tags$li(tags$b("Day:"), "Day of the month of the last contact of the current marketing campaign"),
+        tags$li(tags$b("Month:"), "Month of the last contact of the current marketing campaign"),
+        tags$li(tags$b("Duration:"), "Duration (in seconds) of the last contact of the current marketing campaign"),
+        tags$li(tags$b("Campaign:"), "Number of contacts performed during this marketing campaign and for this target"),
+        tags$li(tags$b("PDays:"), "Number of days that passed by after the target was last contacted from a previous marketing campaign (-1 means target was not contacted previously)"),
+        tags$li(tags$b("Previous:"), "Number of contacts performed before this marketing campaign and for this target"),
+        tags$li(tags$b("POutcome:"), "Outcome of the previous marketing campaign before this campaign for this target"),
+        tags$li(tags$b("Success:"), "Outcome of this marketing campaign for a term deposit")
+      ),
+      p(
+        "More Information:",
+        a(
+          href = "https://www.kaggle.com/datasets/prakharrathi25/banking-dataset-marketing-targets",
+          "Banking Dataset - Marketing Targets"
+        )
+      ),
+    )
+  })
+  
   ### Data Download Functionality
   output$download <- renderUI({
     tagList(
