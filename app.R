979a0b54ed43f51a462887b6ea500f760769d009
Thad Creech <thcreech51@gmail.com>
2025-11-05 15:07:14 -0500

    Finished side panel functionality

--- app.R
+++ app.R
@@ -1,224 +1,119 @@
+
 library(shiny)
+library(shinyWidgets)
 library(shinyalert)
 library(tidyverse)
 
 source("helpers.R")
 
+# Load data and obtain variable lists
+
+all_data <- load_bank_data()
+cat_vars <- names(select(all_data, where(is.factor)))
+num_vars <- names(select(all_data, where(is.numeric)))
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-  h2("Correlation Exploration"),
+  h2("Bank Marketing Data Explorer"),
   sidebarLayout(
     sidebarPanel(
-      h2("Select Variables to Find Correlation:"),
+      h2("Choose filter on panel below then click Apply."),
+      # Categorical 1
       selectizeInput(
-        "corr_x", "X Variable",
-        choices = numeric_vars,
-        selected = numeric_vars[1],
-        options = list(placeholder = "Choose X")
+        "cat1", "Filter Category 1",
+        choices = cat_vars,
+        selected = cat_vars[1],
+        options = list(placeholder = "Filter Category 1")
       ),
+      uiOutput("cat_levels1"),
+      # Categorical 2
       selectizeInput(
-        "corr_y", "Y Variable",
-        choices = numeric_vars,
-        selected = numeric_vars[2],
-        options = list(placeholder = "Choose Y")
+        "cat2", "Filter Category 2",
+        choices = cat_vars,
+        selected = cat_vars[2],
+        options = list(placeholder = "Filter Category 2")
       ),
-      h2("Choose a subset of the data:"),
-      radioButtons(
-        "hhl_corr", "Household Language",
-        choiceNames = c("All", "English only", "Spanish", "Other"),
-        choiceValues = c("all", "english", "spanish", "other"),
-        selected = "all"
+      uiOutput("cat_levels2"),
+      # Numerical 1
+      selectizeInput(
+        "num1", "Filter Numerical 1",
+        choices = num_vars,
+        selected = num_vars[1],
+        options = list(placeholder = "Filter Numerical 1"),
       ),
-      radioButtons(
-        "fs_corr", "SNAP Recipient",
-        choiceNames = c("All", "Yes", "No"),
-        choiceValues = c("all", "yes", "no"),
-        selected = "all"
-      ),
-      radioButtons(
-        "schl_corr", "Educational attainment",
-        choiceNames = c("All", "High School not Completed", "High School or GED", "College Degree"),
-        choiceValues = c("all", "no_hs", "hs", "college"),
-        selected = "all"
-      ),
-      h2("Select a Sample Size"),
-      sliderInput(
-        "corr_n", NULL,
-        min = 20,
-        max = 500,
-        value = 20,
-        step = 1
-      ),
-      actionButton("corr_sample","Get a Sample!")
+      uiOutput("num_range1"),
+      # Numerical 2
+      selectizeInput(
+        "num2", "Filter Numerical 2",
+        choices = num_vars,
+        selected = num_vars[2],
+        options = list(placeholder = "Filter Numerical 2"),
+      ), 
+      uiOutput("num_range2"),
+      actionButton("apply","Apply")
     ),
     mainPanel(
-      plotOutput("corr_plot", height = "420px"),
-      conditionalPanel("input.corr_sample", #only show if a sample has been taken
-                       h2("Guess the correlation!"),
-                       column(6, 
-                              numericInput("corr_guess",
-                                           "",
-                                           value = 0,
-                                           min = -1, 
-                                           max = 1
-                              )
-                       ),
-                       column(6, 
-                              actionButton("corr_submit", "Check Your Guess!"))
+      tabsetPanel(
+        tabPanel("About", uiOutput("about")),
+        tabPanel("Data Download", uiOutput("download")),
+        tabPanel("Data Exploration", uiOutput("explore"))
       )
     )
   )
 )
 
-my_sample <- readRDS("my_sample_temp.rds")
 
-# Define server logic required to draw a histogram
 server <- function(input, output, session) {
-
+  ### Code for dropdowns and sliders
+  output$cat_levels1 <- renderUI({
+    req(input$cat1)
+    lvls <- sort(unique(all_data[[input$cat1]]))
+    pickerInput(
+      inputId = "cat_vals1",
+      choices = lvls,
+      selected = lvls,
+      multiple = TRUE,
+      options = list(`actions-box` = TRUE)
+    )
+  })
+  
+  output$cat_levels2 <- renderUI({
+    req(input$cat2)
+    lvls <- sort(unique(all_data[[input$cat2]]))
+    pickerInput(
+      inputId = "cat_vals2",
+      choices = lvls,
+      selected = lvls,
+      multiple = TRUE,
+      options = list(`actions-box` = TRUE)
+    )
+  })
+  
+  output$num_range1 <- renderUI({req(input$num1); make_slider(all_data, input$num1, "num_rng1")})
+  output$num_range2 <- renderUI({req(input$num2); make_slider(all_data, input$num2, "num_rng2")})
+  
+  ### Code for Apply Button
+  
+  filtered <- reactiveValues(data = all_data)
+  
+  observeEvent(input$apply, {
+    data_table <- all_data
     
-    #################################################
-    ##Correlation tab
-    #This code makes sure the select boxes update so they can't select the same variable in both!
-    #first, update the 'y' selections available
-    observeEvent(input$corr_x, {
-      corr_x <- input$corr_x
-      corr_y <- input$corr_y
-      choices <- numeric_vars
-      if (corr_x != corr_y){
-        choices <- choices[-which(choices == corr_x)]
-        updateSelectizeInput(session,
-                             "corr_y",
-                             choices = choices,
-                             selected = corr_y)
-      }
-    })
-    #now, update the 'x' selections available
-    observeEvent(input$corr_y, {
-      corr_x <- input$corr_x
-      corr_y <- input$corr_y
-      choices <- numeric_vars
-      if (corr_x != corr_y){
-        choices <- choices[-which(choices == corr_y)]
-        updateSelectizeInput(session,
-                             "corr_x",
-                             choices = choices,
-                             selected = corr_x)
-      }
-    })
+    # Filter Categories
+    data_table <- data_table |> filter(.data[[input$cat1]] %in% input$cat_vals1)
+    data_table <- data_table |> filter(.data[[input$cat2]] %in% input$cat_vals2)
     
-
-
-
-    #Create a reactiveValues() object called sample_corr
-    #this object should have two elements, corr_data and corr_truth
-    #both should be set to null to start with!
-    sample_corr <- reactiveValues(corr_data = NULL, corr_truth = NULL)
-
-
-
-    # ##############################################################
-    # #Uncomment the next large block of code to go in an
-    # #observeEvent() to look for the action button (corr_sample)
-    # #Note you can highlight and bulk comment/uncomment (ctrl+shift+c or similar on mac)
-    observeEvent(input$corr_sample, {
-      if(input$hhl_corr == "all"){
-        hhl_sub <- HHLvals
-      } else if(input$hhl_corr == "english"){
-        hhl_sub <- HHLvals["1"]
-      } else if(input$hhl_corr == "spanish"){
-        hhl_sub <- HHLvals["2"]
-      } else {
-        hhl_sub <- HHLvals[c("0", "3", "4", "5")]
-      }
-
-      if(input$fs_corr == "all"){
-        fs_sub <- FSvals
-      } else if(input$fs_corr == "yes"){
-        fs_sub <- FSvals["1"]
-      } else {
-        fs_sub <- FSvals["2"]
-      }
-
-      if(input$schl_corr == "all"){
-        schl_sub <- SCHLvals
-      } else if(input$schl_corr == "no_hs"){
-        schl_sub <- SCHLvals[c("0", "01", "02", "03", "04",
-                               "05", "06", "07", "08", "09",
-                               "10", "11", "12", "13", "14", "15")]
-      } else if(input$schl_corr == "hs"){
-        schl_sub <- SCHLvals[as.character(16:19)]
-      } else {
-        schl_sub <- SCHLvals[as.character(20:24)]
-      }
-
-      corr_vars <- c(input$corr_x, input$corr_y)
-
-      subsetted_data <- my_sample |>
-        filter(#cat vars first
-          HHLfac %in% hhl_sub,
-          FSfac %in% fs_sub,
-          SCHLfac %in% schl_sub
-        ) %>% #make sure numeric variables are in appropriate range, must use %>% here for {} to work
-        {if("WKHP" %in% corr_vars) filter(., WKHP > 0) else .} %>%
-        {if("VALP" %in% corr_vars) filter(., !is.na(VALP)) else .} %>%
-        {if("TAXAMT" %in% corr_vars) filter(., !is.na(TAXAMT)) else .} %>%
-        {if("GRPIP" %in% corr_vars) filter(., GRPIP > 0) else .} %>%
-        {if("GASP" %in% corr_vars) filter(., GASP > 0) else .} %>%
-        {if("ELEP" %in% corr_vars) filter(., ELEP > 0) else .} %>%
-        {if("WATP" %in% corr_vars) filter(., WATP > 0) else .} %>%
-        {if("PINCP" %in% corr_vars) filter(., AGEP > 18) else .} %>%
-        {if("JWMNP" %in% corr_vars) filter(., !is.na(JWMNP)) else .}
-
-        index <- sample(1:nrow(subsetted_data),
-                        size = input$corr_n,
-                        replace = TRUE,
-                        prob = subsetted_data$PWGTP/sum(subsetted_data$PWGTP))
-        # #***You now need to update the sample_corr reactive value object***
-        #the corr_data argument should be updated to be the subsetted_data[index,]
-        #the corr_truth argument should be updated to be the correlation between
-        #the two variables selected. This can be found with this code:
-        sample_corr$corr_data <- subsetted_data[index,]
-        sample_corr$corr_truth <- cor(sample_corr$corr_data |> select(corr_vars))[1,2]
-    })
-    ####################################################################
-
-
-
-    # #Create a renderPlot() object to output a scatter plot
-    # #Use the code below to validate that data exists, (this goes in the renderPlot and you'll need 
-    # #to install the shinyalert package if you don't have it) and then create the appropriate
-    # #scatter plot
-      output$corr_plot <- renderPlot({
-        validate(
-          need(!is.null(sample_corr$corr_data), "Please select your variables, subset, and click the 'Get a Sample!' button.")
-          )
-          ggplot(sample_corr$corr_data, aes_string(x = isolate(input$corr_x), y = isolate(input$corr_y))) +
-            geom_point()
-      })
-
-
-    #This code does the correlation guessing game! Nothing to change here
-    observeEvent(input$corr_submit, {
-      close <- abs(input$corr_guess - sample_corr$corr_truth) <= .05
-      if(close){
-        shinyalert(title = "Nicely done!",
-                   paste0("The sample correlation is ", 
-                          round(sample_corr$corr_truth, 4), 
-                          "."),
-                   type = "success"
-        )
-      } else {
-        if(input$corr_guess > sample_corr$corr_truth){
-          shinyalert(title = "Try again!",
-                     "Try guessing a lower value.")
-        } else {
-          shinyalert(title = "Try again!",
-                     "Try guessing a higher value.")
-        }
-      }
-    })
+    # Filter Numerics
+    rng1 <- input$num_rng1
+    rng2 <- input$num_rng2
+    data_table <- data_table |> filter(.data[[input$num1]] >= rng1[1] & .data[[input$num1]] <= rng1[2])
+    data_table <- data_table |> filter(.data[[input$num2]] >= rng2[1] & .data[[input$num2]] <= rng2[2])
     
+    filtered$data <- data_table
     
+  })
+  
+
 }
 
 # Run the application 
